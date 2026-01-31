573130322521ca07f938d61890b3e883f421933a
SamirELBOUKAOUI <samir.elboukaoui@vnb-it.fr>
2026-01-31 17:20:18 +0100

    ex00 DONE

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -1,18 +1,21 @@
 #include <iostream>
 #include <algorithm>
 #include <iterator>
-
+#include <stdexcept>
+#include <vector>
+#include <list>
+#include <deque>
+#include <set>
 
 template <typename T>
-
 int easyfind(const T &a, int n)
 {
-    vector<int> idx =  0;
-    idx = std::find(a.begin(), a.end(), n);
-    if (idx)
-        return idx;
 
-    return -1;
+    typename T::const_iterator tofind = std::find(a.begin(), a.end(), n);
+    if (tofind == a.end())
+        throw std::runtime_error("Vlaue not found in the container");
+
+    return std::distance(a.begin(), tofind);
 }
 
 
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,8 +1,28 @@
-
 #include "easyfind.hpp"
+
 
 int main()
 {
-    vector<int> z = {1,2,3,4,5,6};
-    easyfind(z, 3);
+    int arr[] = {5,6,222,42,0,4};
+
+    std::vector<int> v(arr, arr+6);
+
+    std::list<int> l(arr, arr+6);
+
+    std::deque<int> d(arr, arr+6);
+
+    std::set<int> s(arr, arr+6);
+
+
+    try {
+        std::cout << "Vector: index = " << easyfind(v, 42) << std::endl;
+        std::cout << "List: index = " << easyfind(l, 42) << std::endl;
+        std::cout << "Deque: index = " << easyfind(d, 42) << std::endl;
+        std::cout << "Set: index = " << easyfind(s, 42) << std::endl;
+        //not found case:
+        std::cout << "Vector: index = " << easyfind(v, 677202002) << std::endl;
+    }
+    catch (std::exception &e) {
+        std::cout << "Exception caught: " << e.what() << std::endl;
+    }
 }
