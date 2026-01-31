3dd83ede8a14432ebaba731c52692d14d6ac9f76
Selboukaoui <samirelboukaoui12@gmail.com>
2026-01-31 10:01:22 +0100

    start

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -5,12 +5,20 @@
 
 template <typename T>
 
-int easyfind(T a, int n)
+int easyfind(const T &a, int n)
 {
     vector<int> idx =  0;
-    isx = std::find(a.begin(), a.end(), n);
+    idx = std::find(a.begin(), a.end(), n);
     if (idx)
         return idx;
 
     return -1;
 }
+
+
+
+
+
+
+
+
